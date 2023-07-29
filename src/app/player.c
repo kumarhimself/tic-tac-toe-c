89adae1624fc21cfdf119801ab66a6ca4615c38b
Ayush Kumar Singh <ayush.k@outlook.com>
2023-07-28 20:36:37 -0400

    Added more input rules and corrected errors relating with user providing inputs

--- src/app/player.c
+++ src/app/player.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+
 #include "player.h"
 
 // Function Prototype
-bool valid_input(char *board, int *position);
+void is_valid_input(char *prompt, int *scan_var);
+bool is_position_taken(char *board, int *position);
 
 char player_marker() {
 	static int num_turns = -1;
@@ -15,18 +19,33 @@ char player_marker() {
 
 void player_input(char *board, int *position) {
 	while (true) {
-		printf("Please enter a position number: \n");
-		scanf("%d", position);
+		char *prompt = malloc(sizeof(char) * 20);
+		strcpy(prompt, "Please enter a position number: ");	
+		is_valid_input(prompt, position);
 
-		if (!valid_input(board, position)) {
-			printf("Invalid input, please try again!\n");
+		if (!is_position_taken(board, position)) {
+			printf("This position is already taken, please try again!\n");
 		} else {
 			break;
 		}
 	}
 }
 
-bool valid_input(char *board, int *position) {
+void is_valid_input(char *prompt, int *scan_var) {
+	while (true) {
+		printf("%s\n", prompt);
+		int num_read = scanf("%d", scan_var);
+
+		if (num_read != 1) {
+			printf("Oops, you entered an incorrect input, please try again!\n");
+			fflush(stdin);
+		} else {
+			break;
+		}	
+	}
+}
+
+bool is_position_taken(char *board, int *position) {
 	char value = *(board + *position - 1); 
 	if (value == 'x' || value == 'o') {
 		return false;
--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "app/game.h"
 
 int main(int argc, char *argv[]) {
 	game();
-	
+
 	return 0;
 }
